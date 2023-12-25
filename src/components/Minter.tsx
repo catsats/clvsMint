0bdc5ba11c9020a7b7e5801df50b6ca8788f3f3a
ben jie ming <>
2023-12-25 20:56:46 +0800

    提交通过测试网测试的代码clvs

---
+++ src/components/Minter.tsx
@@ -0,0 +1,181 @@
+"use client";
+
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Keyring, ApiPromise, WsProvider, HttpProvider } from "@polkadot/api";
+import cloverTypes from "@clover-network/node-types";
+//主网
+// const network = getNetworkInfo(Network.Mainnet);
+
+let polkadotlWSS = "https://rpc-ivy-3.clover.finance";
+// const polkadotProvider = new WsProvider(polkadotlWSS);
+const network = new HttpProvider(polkadotlWSS);
+
+function getNowTime() {
+  var d = new Date(),
+    str = "[";
+  str += d.getHours() + ":";
+  str += d.getMinutes() + ":";
+  str += d.getSeconds() + ":";
+  str += d.getMilliseconds();
+  str += "] ";
+  return str;
+}
+
+const Minter: React.FC = () => {
+  const [mnemonic, setMnemonic] = useState<string>("");
+  const [isEnd, setIsEnd] = useState<boolean>(false);
+  const isEndRef = useRef<boolean>(false);
+  isEndRef.current = isEnd;
+  const [logs, setLogs] = useState<string[]>([]);
+  const [count, setCount] = useState<number>(0);
+  const [dotatime, setDotatime] = useState<number>(3000);
+  const [dataText, setDataText] = useState<string>("");
+  const [sendAddress, setSendAddress] = useState<string>("");
+
+  const mintFn = useCallback(
+    async (polkadotApi: any, joeyAddress: any, joey: any, dataText: string, sendAddress:string) => {
+      try {
+        let tx1 = polkadotApi.tx.balances.transferKeepAlive(sendAddress, 30);
+        let tx2 = polkadotApi.tx.system.remark(dataText);
+        const nonce = await polkadotApi.rpc.system.accountNextIndex(
+          joeyAddress
+        );
+        let tx3 = await polkadotApi.tx.utility
+          .batchAll([tx1, tx2])
+          .signAndSend(joey, { nonce });
+        console.log(tx3, "hash:", tx3.hash.toHex());
+        console.log(getNowTime(), "发送交易");
+        if (tx3.hash.toHex().length !== 0) {
+          setCount((prev) => prev + 1);
+          setLogs((pre) => [...pre, `${getNowTime()} ：铸造完成`]);
+        }
+      } catch (e) {
+        // sleep 1s
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+    },
+    []
+  );
+
+  const walletMint = useCallback(
+    async (m: string) => {
+      // const wallet = await generateWalletFromMnemonic(m);
+      const denom = "dot";
+
+      const polkadotApi = await ApiPromise.create({
+        provider: network,
+        types: cloverTypes,
+      });
+      let joey = (new Keyring({ type: "sr25519" }) as any).addFromMnemonic(m);
+      let joeyAddress = joey.address;
+
+      setLogs((pre) => [...pre, `成功导入钱包: ${joeyAddress}`]);
+
+      while (true) {
+        if (isEndRef.current) {
+          setLogs((pre) => [...pre, `暂停铸造`]);
+          break;
+        }
+        await mintFn(polkadotApi, joeyAddress, joey, dataText, sendAddress);
+        await new Promise((resolve) => setTimeout(resolve, dotatime));
+      }
+    },
+    [mintFn, dotatime, dataText, sendAddress]
+  );
+
+  const handleMint = async () => {
+    setIsEnd(false);
+    setLogs((pre) => [...pre, `开始铸造`]);
+
+    // 验证助记词
+    if (!mnemonic) {
+      setLogs((pre) => [...pre, `请输入助记词`]);
+      return;
+    }
+    if (dataText.length == 0) {
+      setLogs((pre) => [...pre, `请输入铭文`]);
+      return;
+    }
+    if(sendAddress.length == 0) {
+      setLogs((pre) => [...pre, `请输入发送地址`]);
+      return;
+    }
+    const walletMnemonics = mnemonic.split(",");
+    for (let i = 0; i < walletMnemonics.length; i++) {
+      walletMint(walletMnemonics[i]);
+    }
+  };
+
+  const handleEnd = () => {
+    setIsEnd(true);
+    isEndRef.current = true;
+  };
+
+  //修改时间暂停所有铸造操作
+  useEffect(() => {
+    handleEnd();
+  }, [dotatime]);
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1>clvs疯狂铸造脚本 交易详细看 https://clover.subscan.io/</h1>
+      <p className="text-xs mt-2 text-gray-400">打到账户没钱为止</p>
+      <div>
+        <textarea
+          className="mt-6 border border-black rounded-xl w-[400px] px-4 py-6 resize-none h-[220px]"
+          placeholder="请输入助记词，比如：jazz bench loan chronic ready pelican travel charge lunar pear detect couch。当有多的账号的时候，用,分割，比如:jazz bench loan chronic ready pelican travel charge lunar pear detect couch,black clay figure average spoil insane hire typical surge still brown object"
+          value={mnemonic}
+          onChange={(e) => setMnemonic(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1>铭文（选填，原始铭文，不是转码后的十六进制）</h1>
+        <input
+          className="mt-6 border border-black rounded-xl w-[400px] px-4 py-6 resize-none h-[40px]"
+          placeholder="铭文，不要输入错了，多检查下，例子：data:,{p:asc-20,op:mint,tick:aval,amt:100000000}"
+          value={dataText}
+          onChange={(e) => setDataText(String(e.target.value))}
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1>设置发送间隔时长 1000 = 1秒</h1>
+        <input
+          className="mt-6 border border-black rounded-xl w-[400px] px-4 py-6 resize-none h-[40px]"
+          placeholder="请输入发送间隔时长，单位毫秒"
+          value={dotatime}
+          onChange={(e) => setDotatime(Number(e.target.value))}
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1>30CLV 自转填自己地址，转项目方填项目方地址</h1>
+        <input
+          className="mt-6 border border-black rounded-xl w-[400px] px-4 py-6 resize-none h-[40px]"
+          placeholder="自转填自己地址，转项目方填项目方地址"
+          value={sendAddress}
+          onChange={(e) => setSendAddress(String(e.target.value))}
+        />
+      </div>
+      <div className="flex w-[400px] justify-center space-x-6 mt-4">
+        <button
+          className="border border-black px-4 py-2 rounded-full"
+          onClick={handleMint}
+        >
+          开始铸造
+        </button>
+        <button
+          className="border border-black px-4 py-2 rounded-full"
+          onClick={handleEnd}
+        >
+          暂停
+        </button>
+      </div>
+
+      <span className="mt-6 w-[400px] text-left">{`日志(本次已铸造+${count})`}</span>
+      <div className="px-4 py-2 whitespace-pre border border-black w-[400px] h-[400px] overflow-auto">
+        {logs.join("\n")}
+      </div>
+    </div>
+  );
+};
+
+export default Minter;
